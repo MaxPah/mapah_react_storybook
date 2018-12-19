a07ea8c6006afee32d80e0832a6667d303d86966
Maxime PAHUD <maxime.pahud@smile.fr>
2018-12-19 22:09:27 +0100

    Add paragraph

--- src/components/1-Atoms/PgfSlider/PgfSlider.js
+++ src/components/1-Atoms/PgfSlider/PgfSlider.js
@@ -19,17 +19,17 @@ export default function PgfSlider({ title, slides}) {
     );
 }
 
-new Siema({
-    selector: '.siema',
-    duration: 200,
-    easing: 'ease-out',
-    perPage: 1,
-    startIndex: 0,
-    draggable: true,
-    multipleDrag: true,
-    threshold: 20,
-    loop: false,
-    rtl: false,
-    onInit: () => { },
-    onChange: () => { },
-});
+// new Siema({
+//     selector: '.siema',
+//     duration: 200,
+//     easing: 'ease-out',
+//     perPage: 1,
+//     startIndex: 0,
+//     draggable: true,
+//     multipleDrag: true,
+//     threshold: 20,
+//     loop: false,
+//     rtl: false,
+//     onInit: () => { },
+//     onChange: () => { },
+// });
