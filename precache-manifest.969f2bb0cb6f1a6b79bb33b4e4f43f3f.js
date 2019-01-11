0b70f0ba3fd4d8f807dac36656455843cfc94e1a
Maxime PAHUD <maxime.pahud@smile.fr>
2019-01-11 21:47:09 +0100

    Updates

---
+++ precache-manifest.969f2bb0cb6f1a6b79bb33b4e4f43f3f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/mapah_react_storybook/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "7626dd3c2a2c0338abcf",
+    "url": "/mapah_react_storybook/static/js/runtime~main.7626dd3c.js"
+  },
+  {
+    "revision": "dcaed2989e2dc3e50efd",
+    "url": "/mapah_react_storybook/static/js/main.dcaed298.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/mapah_react_storybook/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "dcaed2989e2dc3e50efd",
+    "url": "/mapah_react_storybook/static/css/main.b437c049.chunk.css"
+  },
+  {
+    "revision": "0a78db8a8403342c288291360daf82e6",
+    "url": "/mapah_react_storybook/index.html"
+  }
+];
